8fc178de17ecd8acd5c2bb71fe4c6c8e70a3e2a1
aliali2131998 <40716889+aliali2131998@users.noreply.github.com>
2023-11-18 10:35:13 +0400

    Update server.js

--- server.js
+++ server.js
@@ -16,5 +16,4 @@ io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
-
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
